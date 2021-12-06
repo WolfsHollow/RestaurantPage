8ec0a2408462c590eb4d8e86242237355aee6c34
Wulfes <wolvenhollow@gmail.com>
2021-12-06 14:04:33 -0600

    Added tab switching, revamped homepage,  changed background/tabs

--- src/index.js
+++ src/index.js
@@ -1,7 +1,44 @@
 import './style.css';
-import loadPageDiv from './titlePage.js';
+import home from './titlePage.js';
 import menu from './menu.js';
 
 const body = document.getElementById('content');
-// body.appendChild(loadPageDiv());
-body.appendChild(menu);
+
+function createNavBar(){
+    const navBar = document.createElement('div');
+    const menuTab = document.createElement('div');
+    const homeTab = document.createElement('div');
+    const contactTab = document.createElement('div');
+
+    navBar.classList.add('nav');
+    menuTab.classList.add('tab');
+    homeTab.classList.add('tab');
+    contactTab.classList.add('tab');
+
+    menuTab.textContent = 'Menu';
+    homeTab.textContent = 'Home';
+    contactTab.textContent = 'Contact';
+
+    navBar.appendChild(homeTab);
+    navBar.appendChild(menuTab);
+    navBar.appendChild(contactTab);
+
+    menuTab.addEventListener('click', () => {switchToPage(menu)});
+    homeTab.addEventListener('click', () => {switchToPage(home)});
+    contactTab.addEventListener('click', () => {switchToPage(contact)});
+
+    return navBar;
+}
+
+function removeCurrentPage(){
+    let currentPage = body.lastChild;
+    body.removeChild(currentPage);
+}
+
+function switchToPage(tab){
+    removeCurrentPage();
+    body.appendChild(tab);
+}
+
+body.appendChild(createNavBar());
+body.appendChild(home);
--- src/titlePage.js
+++ src/titlePage.js
@@ -1,19 +1,31 @@
-export default function loadPageDiv() {
+import image from './restaurant.png';
+
+function createHomePage() {
 
     const pageWrapper = document.createElement('div');
+    pageWrapper.classList.add('home');
     const title = document.createElement('div');
-    title.classList.add('title');
+    title.classList.add('homeTitle');
     const imgDiv = document.createElement('div');
-    imgDiv.classList.add('imgDiv');
+    imgDiv.classList.add('homeImgDiv');
+    
+    const img = document.createElement('img');
+    img.classList.add('homeImg');
+    img.src = image;
+
     const description = document.createElement('div');
-    description.classList.add('description');
+    description.classList.add('homeDescription');
 
     pageWrapper.appendChild(title);
     pageWrapper.appendChild(imgDiv);
     pageWrapper.appendChild(description);
+    imgDiv.appendChild(img);
 
-    title.textContent = 'Restaurant Page';
+    title.textContent = 'Fantasy Restaurant';
     description.textContent = 'Hello this is stuff about this restaurant. Hello this is stuff about this restaurant. Hello this is stuff about this restaurant. Hello this is stuff about this restaurant. Hello this is stuff about this restaurant.';
  
     return pageWrapper;
 }
+
+const home = createHomePage();
+export default home;
